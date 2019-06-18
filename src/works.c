648d49b0d1e0bc398cf9dba46145ed7ec20e1e06
M.tomoya <m-t2000@outlook.jp>
2019-06-19 03:36:20 +0900

    work完成

--- src/works.c
+++ src/works.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  int cnt = 0;
+  for (int j = 0; j < n; j++){
+    if (A[j] > m) return 0;
+    if (sum + A[j] > m){
+      sum = A[j];
+      cnt++;
+    }else{
+      sum += A[j];
+    }
+  }
+  return cnt < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +26,15 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000001;
+  while (ub - lb > 1){
+    int m = (ub + lb) / 2;
+    if (p(m)) ub = m;
+    else lb = m;
+  }
+
+  printf("%d\n",ub);
 
   return 0;
 }
